9bff68f005b7b30e6d0056b950033fb6555f9f88
lihuang3 <lihuang.mech@gmail.com>
2018-03-12 15:19:49 -0500

    Add files via upload

---
+++ src/follower_color_filter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# BEGIN ALL
+import rospy, cv2, cv_bridge, numpy
+from sensor_msgs.msg import Image
+
+class Follower:
+  def __init__(self):
+    self.bridge = cv_bridge.CvBridge()
+    self.image_sub = rospy.Subscriber('camera/rgb/image_raw', 
+                                      Image, self.image_callback)
+  def image_callback(self, msg):
+    cv2.namedWindow("window1", 1)
+    cv2.namedWindow("window2", 1)
+    # BEGIN BRIDGE
+    image = self.bridge.imgmsg_to_cv2(msg)
+    # END BRIDGE
+    # BEGIN HSV
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # END HSV
+    # BEGIN FILTER
+    lower_yellow = numpy.array([ 10,  10, 10])
+    upper_yellow = numpy.array([255, 255, 250])
+    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    # END FILTER
+    masked = cv2.bitwise_and(image, image, mask=mask)
+    cv2.imshow("window1", mask ) 
+    cv2.imshow("window2",hsv)
+    cv2.waitKey(3)
+
+
+rospy.init_node('follower')
+follower = Follower()
+rospy.spin()
+# END ALL
